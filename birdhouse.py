bf36380d460115d4f73e9d400f5b5bfff0e1280d
Jaracah Teague <jaracahteague@JKT-MB.local>
2022-02-24 13:21:26 -0500

    progress on sending email

--- birdhouse.py
+++ birdhouse.py
@@ -55,26 +55,26 @@ def send_email():
 	# add the attachment (image) to the message
 	# filename = ''
 	with open(filename, "rb") as attachment:
-    	part = MIMEBase("application", "octet-stream")
-    	part.set_payload(attachment.read())
+		part = MIMEBase("application", "octet-stream")
+		part.set_payload(attachment.read())
 
-    # encoding
+	# encoding
 	encoders.encode_base64()
 
 	part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-    )
+	"Content-Disposition",
+	f"attachment; filename= {filename}",
+	)
 
 	message.attach(part)
-    """
+	"""
 	
-   # sending the message
+	# sending the message
 	text = message.as_string()
 	ontext = ssl.create_default_context()
 	with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    	server.login(sender_email, password)
-    	server.sendmail(sender_email, receiver_email, text)
+		server.login(sender_email, password)
+		server.sendmail(sender_email, receiver_email, text)
 
 # Main section
 
