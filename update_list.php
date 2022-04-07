1bff476ab68278a622e397529785037651f8779c
teaguejk <teaguejk@appstate.edu>
2022-04-07 15:31:50 -0400

    update

--- update_list.php
+++ update_list.php
@@ -1,14 +1,35 @@
 <?php
-    $keys = array('email');
-    $csv_line = array();
-    foreach($keys as $key){
-        array_push($csv_line,'' . $_GET[$key]);
-    }
-    $fname = 'mailing_list.csv';
-    $csv_line = implode(',',$csv_line);
-    if(!file_exists($fname)){$csv_line = "\r\n" . $csv_line;}
-    $fcon = fopen($fname,'a');
-    $fcontent = $csv_line;
-    fwrite($fcon,$csv_line);
-    fclose($fcon);
+
+if (isset($_POST['submit'])) {
+
+   //collect form data
+   $email = $_POST['email'];
+
+   //if no errors carry on
+   if (!isset($error)) {
+
+      // Title of the CSV
+      $Content = "Email";
+
+      //set the data of the CSV
+      $Content. = "$email";
+
+      //set the file name and create CSV file
+      $FileName = "mailing_list1.csv"
+      header('Content-Type: application/csv');
+      header('Content-Disposition: attachment; filename="'.$FileName.
+         '"');
+      echo $Content;
+      exit();
+   }
+}
+
+//if their are errors display them
+if (isset($error)) {
+   foreach($error as $error) {
+      echo '$error';
+   }
+}
+
+
 ?>
