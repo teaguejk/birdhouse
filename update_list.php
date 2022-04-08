bbb67174b7234b5ab229bb4231238101f0f35b64
teaguejk <teaguejk@appstate.edu>
2022-04-07 20:50:16 -0400

    php script working!

--- update_list.php
+++ update_list.php
@@ -1,17 +1,26 @@
-<? php
+<?php
 
-$email = $_POST["email"];
-$keys = array($email);
-$csv_line = $keys;
-foreach( $keys as $key ){
-    array_push($csv_line,'' . $_GET[$key]);
+if (isset($_POST['email'])) {
+
+    $fA = $_POST["email"];
+    //$fB = $_POST["name"];
+    $keys = array($fA);  //, $fB);
+    $csv_line = $keys;
+    foreach( $keys as $key ){
+        array_push($csv_line,'' . $_GET[$key]);
+    }
+    $csv_line = implode(',',$csv_line);
+    $fname = 'mailing_list.csv';
+    if(!file_exists($fname)){$csv_line = $csv_line."\r\n" ;}
+    $fp = fopen($fname,'a');
+    //print_r(error_get_last());
+    //$fcontent = $csv_line;
+    fwrite($fp,$csv_line);
+    fclose($fp);
+    //echo $csv_line;
+    echo "Thank you for subscribing to the mailing list!";
+} else {
+    echo "Empty field submitted.";
 }
-$fname = 'mailing_list.csv';
-$csv_line = implode(',',$csv_line);
-if(!file_exists($fname)){$csv_line = $csv_line."\r\n" ;}
-$fcon = fopen($fname,'a');
-$fcontent = $csv_line;
-fwrite($fcon,$csv_line);
-fclose($fcon);
 
 ?>
