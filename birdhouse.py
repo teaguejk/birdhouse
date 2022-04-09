dc2889780f3e30751d99fd3551396172415d038f
teaguejk <teaguejk@appstate.edu>
2022-04-08 21:41:58 -0400

    mod birdhouse.py

--- birdhouse.py
+++ birdhouse.py
@@ -5,11 +5,21 @@
 # Capstone Project
 # Appalachian State University
 #
+# img/video files stored in ./assets
+# assumes that the student2.cs.appstate.edu server is hosting the site 
+# located at usr/local/apache2/htdocs/u/teaguejk/birdhouse
+# 
+# pi camera related things may be temporarily commented out 
+#
+# emails are sent from zeroDoNotReply@gmail.com' but can be changed
+#
+# passwords are entered on runtime for security
 """
 
-# img/video files stored in ./assets
-# pi camera related things may be temporarily commented out 
+#==========================================================================================
+# Setup
 
+#------------------------------------------------------------------------------------------
 # py imports
 import pandas as pd
 import numpy as np
@@ -29,31 +39,34 @@ from email.mime.text import MIMEText
 
 from fabric.connection import Connection
 
-
 # pi imports 
 # Comment out when not on PI
 # import RPi.GPIO as GPIO
 # from picamera import PiCamera
 # from picamera.array import PiRGBArray
 
+#------------------------------------------------------------------------------------------
 # Settings
+
+#Camera Settings
 camera_delay = 2.5
 resolution = [640, 480]
 fps = 16
 min_area = 5000
 
 # Web Server Setup
-# Mailing list stored in ./mailing_list.csv
 user = 'teaguejk'
 host = 'student2.cs.appstate.edu'
 path = '/usr/local/apache2/htdocs/u/teaguejk/birdhouse'
+# Mailing list is stored in a csv file at /usr/local/apache2/htdocs/u/teaguejk/birdhouse/mailing_list.csv
 
-# Email
+# Sender Email
 sender_email = 'zeroDoNotReply@gmail.com'
-
-
 #------------------------------------------------------------------------------------------
+
+#==========================================================================================
 # Notes
+
 """
 # ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -74,10 +87,74 @@ for f in camera.capture_continuous(raw_capture, format="bgr", use_video_port=Tru
     # time.sleep(2)
     # camera.stop_preview()
 
+# server.sendmail(sender_email, recepient_emails[0], text) # send an email
+
+
 """
 
-#------------------------------------------------------------------------------------------
+#==========================================================================================
 # Function defs
+
+def send_email(epass, spass, filename):
+    """
+    Function to send an email if given conditions are met,
+    this email will contain an image attachment
+    Sends to a list of users
+    
+    """
+    # recipient email list
+    # will eventually be retrieved from website
+    # by reading a csv file into a pandas dataframe
+    from fabric.connection import Connection
+    with Connection(host, user, connect_kwargs={'password': spass, 'allow_agent': False}) as c, c.sftp() as sftp,   \
+         sftp.open(path + '/mailing_list.csv') as file:
+            mailing_list = pd.read_csv(file, skip_blank_lines=True, header=None).T
+    # drop NaN or None type entries
+    mailing_list = mailing_list.dropna() 
+
+    # message setup
+
+    timestamp   = time.strftime('%m-%d-%y %H:%M:%S')
+    subject     = 'Automated Email: Motion Detected in Birdhouse ' + timestamp
+    body        = 'Motion Detected in Birdhouse'
+    message     = MIMEMultipart()
+
+    message["From"]     = sender_email
+    message["To"]       = sender_email
+    message["Subject"]  = subject
+
+    message.attach(MIMEText(body, "plain"))
+    
+    # add the attachment (image) to the message
+    # filename = 'IMG.jpg'
+    with open(filename, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # encoding
+    encoders.encode_base64(part)
+
+    part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+    )
+
+    message.attach(part)
+    
+    # sending the message
+    text = message.as_string()
+    # if certificates cannot be verified, use this context instead of the default
+    # ssl.SSLContext.verify_mode = ssl.VerifyMode.CERT_NONE
+    # context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, epass)
+        # loop through dataframe and send email to each row (address) 
+        for row in mailing_list.iterrows(): 
+                server.sendmail(sender_email, row[1].values, text)
+
+#------------------------------------------------------------------------------------------
+# Works only on PI
 
 def capture_video():
     """
@@ -131,74 +208,11 @@ def capture_image():
 
     pass
 
-
-
-def send_email(epass, spass, filename):
-    """
-    Function to send an email if given conditions are met,
-    this email will contain an image attachment
-    Sends to a list of users
-    
-    """
-    # recipient email list
-    # will eventually be retrieved from website
-    # by reading a csv file into a pandas dataframe
-    from fabric.connection import Connection
-    with Connection(host, user, connect_kwargs={'password': spass, 'allow_agent': False}) as c, c.sftp() as sftp,   \
-         sftp.open(path + '/mailing_list.csv') as file:
-            # mailing_list = np.genfromtxt(file, delimiter=',', header=None)
-            mailing_list = pd.read_csv(file, skip_blank_lines=True, header=None).T
-
-    # print(mailing_list[:])
-
-    recepient_emails = [
-        'teaguejk@appstate.edu',
-        'erinbrzezin@gmail.com'
-    ]
-    
-    # message setup
-
-    timestamp   = time.strftime('%m-%d-%y %H:%M:%S')
-    subject     = 'Automated Email: Motion Detected in Birdhouse ' + timestamp
-    body        = 'Motion Detected in Birdhouse'
-    message     = MIMEMultipart()
-
-    message["From"]     = sender_email
-    message["To"]       = recepient_emails[0]
-    message["Subject"]  = subject
-
-    message.attach(MIMEText(body, "plain"))
-    
-    # add the attachment (image) to the message
-    # filename = 'IMG.jpg'
-    with open(filename, "rb") as attachment:
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-
-    # encoding
-    encoders.encode_base64(part)
-
-    part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-    )
-
-    message.attach(part)
-    
-    # sending the message
-    text = message.as_string()
-    # if certificates cannot be verified, use this context instead of the default
-    # ssl.SSLContext.verify_mode = ssl.VerifyMode.CERT_NONE
-    # context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, epass)
-        server.sendmail(sender_email, recepient_emails[0], text)
-        # for row in mailing_list.iterrows():
-        #     server.sendmail(sender_email, row[0], text)
-
 #------------------------------------------------------------------------------------------
+
+#==========================================================================================
 # Main routine
+
 def main():
     # get passwords on start
     print("For Email: "  + sender_email)
@@ -225,4 +239,5 @@ def main():
 if __name__ == "__main__":
     main()
 
+#==========================================================================================
 
