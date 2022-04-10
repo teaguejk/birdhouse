baff6642bbe08296a4c57c80cc04e6968673033a
teaguejk <teaguejk@appstate.edu>
2022-04-10 19:43:25 -0400

    fixed path

--- upload.py
+++ upload.py
@@ -5,4 +5,4 @@ host = 'student2.cs.appstate.edu'
 path = '/usr/local/apache2/htdocs/u/teaguejk/birdhouse.site'
 filename = './assets/IMG.jpg'
 with Connection(host, user, connect_kwargs={'password': '900728429', 'allow_agent': False}) as c:  
-         c.put(filename, path + "/IMG.jpg")
+         c.put(filename, path + "/assets/IMG.jpg")
