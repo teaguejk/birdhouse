b72d61846d7a5b2d1b7664c0dac7fad8da24a8ac
teaguejk <teaguejk@appstate.edu>
2022-04-10 19:57:06 -0400

    simple upload script in the meantime

--- upload.py
+++ upload.py
@@ -1,8 +1,20 @@
-
+# imports
 from fabric.connection import Connection
+
+# setup
 user = 'teaguejk'
 host = 'student2.cs.appstate.edu'
 path = '/usr/local/apache2/htdocs/u/teaguejk/birdhouse.site'
 filename = './assets/IMG.jpg'
-with Connection(host, user, connect_kwargs={'password': '900728429', 'allow_agent': False}) as c:  
-         c.put(filename, path + "/assets/IMG.jpg")
+
+# main
+def main():
+    print("For server: " + user + "@" + host)
+    spass = input("Server Password:\n")
+
+    with Connection(host, user, connect_kwargs={'password': spass, 'allow_agent': False}) as c:  
+        c.put(filename, path + "/assets/IMG.jpg")
+
+if __name__ == "__main__":
+    main()
+
