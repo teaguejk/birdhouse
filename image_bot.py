97ad8e686edfc872df4a35949d72d206b0aa9c96
teaguejk <teaguejk@appstate.edu>
2022-04-13 13:42:25 -0400

    working bot

--- image_bot.py
+++ image_bot.py
@@ -10,11 +10,13 @@ Sends the most recent image to a discord channel
 import discord
 
 client = discord.Client()
-token = ''
+token = 'OTYzODIxNzc1NTgwNDMwNDE2.Ylbq-g.uP5v0ez9vglFDFWo3qoDqASFMag'
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+	print('Logged in as {0.user}'.format(client))
+	channel = client.get_channel(796549061200314432)
+	await channel.send('hi')
 
 client.run(token)
 
