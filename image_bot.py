9d636b5e41300c4c1add9db8e8af452d7647c283
teaguejk <teaguejk@appstate.edu>
2022-04-14 14:17:16 -0400

    bot mostly done

--- image_bot.py
+++ image_bot.py
@@ -19,14 +19,32 @@ filetype = file_type = r'/*jpg'
 @client.event
 async def on_ready():
 	print('Logged in as {0.user}'.format(client))
-	channel = client.get_channel(796549061200314432)
+
+	# for testing purposes
+	# channel = client.get_channel(796549061200314432)
 	# await channel.send('hi')
 
-	files = glob.glob(path + filetype)
-	recent = max(files, key=os.path.getctime)
-	with open(recent, 'rb') as f:
-	    img = discord.File(f)
-	    await channel.send(file=img)
+	game = discord.Game("watching for birds")
+	await client.change_presence(status=discord.Status.idle, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+
+	if message.author == client.user:
+		return
+
+	if message.content == '!bird':
+		# get the most recent image
+		files = glob.glob(path + filetype)
+		recent = max(files, key=os.path.getctime)
+		with open(recent, 'rb') as f:
+			img = discord.File(f)
+			await message.channel.send(file=img)
+	else:
+		await message.channel.send("Use !bird to display the most recent image...")
+
 
 client.run(token)
 
