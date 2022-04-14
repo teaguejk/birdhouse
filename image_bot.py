ffb61301ae3bc87d4a29d50d82d8daa2bfe8f725
teaguejk <teaguejk@appstate.edu>
2022-04-14 10:04:57 -0400

    bot sends images now

--- image_bot.py
+++ image_bot.py
@@ -8,15 +8,25 @@ Sends the most recent image to a discord channel
 """
 
 import discord
+import os.path
+import glob
 
 client = discord.Client()
 token = ''
+path = './assets/'
+filetype = file_type = r'/*jpg'
 
 @client.event
 async def on_ready():
 	print('Logged in as {0.user}'.format(client))
 	channel = client.get_channel(796549061200314432)
-	await channel.send('hi')
+	# await channel.send('hi')
+
+	files = glob.glob(path + filetype)
+	recent = max(files, key=os.path.getctime)
+	with open(recent, 'rb') as f:
+	    img = discord.File(f)
+	    await channel.send(file=img)
 
 client.run(token)
 
